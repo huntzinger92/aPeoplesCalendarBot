a95b1015a1f46c8ba9f47bd5b1fed0637381feee
Trevor Smith <tts2p4@gmail.com>
2022-06-13 10:23:33 -0500

    twitter bot updates

--- postEventsToTwitter.py
+++ postEventsToTwitter.py
@@ -10,75 +10,122 @@ from env import twitterAccessToken
 from env import twitterAccessTokenSecret
 from nltk.tokenize import sent_tokenize
 
-#authenticate with the twit
+# authenticate with the twit
 auth = tweepy.OAuthHandler(twitterApiKey, twitterApiSecretKey)
 auth.set_access_token(twitterAccessToken, twitterAccessTokenSecret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 # this is copied from the front end repository apc-web, generates URLs for specific events
+
+
 def stringToSlug(str):
-    #credit to https://gist.github.com/codeguy/6684588
+    # credit to https://gist.github.com/codeguy/6684588
     str = re.sub('^\s+|\s+$', '', str)
     str = str.lower()
 
-    #remove accents, swap ñ for n, etc
+    # remove accents, swap ñ for n, etc
     from_ = "àáäâèéëêìíïîòóöôōùúüûñç·/_,:;"
     to = "aaaaeeeeiiiiooooouuuunc------"
     for i in range(0, len(from_)):
         str = re.sub(from_[i], to[i], str)
 
-    str = re.sub('[^a-z0-9 -]', '', str) #remove invalid chars
-    str = re.sub('\s+', '-', str) #collapse whitespace and replace by -
-    str = re.sub('-+', '-', str) #collapse dashes
+    str = re.sub('[^a-z0-9 -]', '', str)  # remove invalid chars
+    str = re.sub('\s+', '-', str)  # collapse whitespace and replace by -
+    str = re.sub('-+', '-', str)  # collapse dashes
 
     return str
 
-# todayEvents: apcEvent[], testMode: boolean
+def uploadPhoto(imgSrc):
+    url = 'https://www.apeoplescalendar.org{}'.format(imgSrc)
+    imgTuple = urllib.request.urlretrieve(url)
+    image = imgTuple[0]
+    try:
+        media = api.media_upload(image)
+    except Exception as error:
+        raise Exception(error)
+    return media
+
+def postEventToTwitter(apcEvent):
+    tweetBody, title, description, imgSrc, NSFW, imgAltText = itemgetter(
+            'otd', 'title', 'description', 'imgSrc', 'NSFW', 'imgAltText')(apcEvent)
+    slugifiedEventName = stringToSlug(title)
+    tweetBody += " https://www.apeoplescalendar.org/calendar/events/" + slugifiedEventName
+
+    topLevelTweet = ''
+    media = False
+    # if we have image
+    if imgSrc:
+        try:
+            media = uploadPhoto(imgSrc)
+        except Exception as error:
+            print('could not upload image:')
+            print(imgSrc)
+            print(error)
+            print(media)
+            print('retrying image upload...')
+            try:
+                media = uploadPhoto(imgSrc)
+            except Exception as error:
+                print('could not upload image on 2nd attempt:')
+                print(imgSrc)
+                print(error)
+                print(media)
+                print('yeah, your image is fucked')
+                        
+        if imgAltText:
+            try:
+                api.create_media_metadata(
+                    media.media_id, alt_text=imgAltText)
+            except Exception as error:
+                print('could not upload media metadata:')
+                print(error)
+                print('imgSrc:')
+                print(imgSrc)
+
+        try: topLevelTweet = api.update_status(status=tweetBody, media_ids=[
+                                                media.media_id], possibly_sensitive=NSFW)
+        except Exception as error:
+            # bug in the Tweepy where some image files can't be found
+            print(
+                'posting event photo to twitter caused error, posting text instead')
+            print(error)
+            print('imgSrc:')
+            print(imgSrc)
+            # just do the description
+            topLevelTweet = api.update_status(
+                status=tweetBody, possibly_sensitive=NSFW)
+    else:
+        # if no image, just do the description
+        topLevelTweet = api.update_status(
+            status=tweetBody, possibly_sensitive=NSFW)
+    try:
+        postDescriptionThread(topLevelTweet.id, description, tweetBody)
+    except Exception as e:
+        print(
+            'something horrible happened when trying to post description thread:')
+        print(tweetBody)
+        print(e)
+
 def postEventsToTwitter(todayEvents, testMode):
     for apcEvent in todayEvents:
-        tweetBody, title, description, imgSrc, NSFW, imgAltText = itemgetter('otd', 'title', 'description', 'imgSrc', 'NSFW', 'imgAltText')(apcEvent)
-        # do the submit stuff for every event in the category
-        slugifiedEventName = stringToSlug(title)
-        if len(tweetBody) > 238:
-            print (tweetBody)
-            print('tweet too long! please shorten otd statement in database and try again')
+        otd = itemgetter('otd')(apcEvent)
+        if len(otd) > 238:
+            print(otd)
+            print(
+                'tweet too long! please shorten otd statement in database and try again')
             return
-        tweetBody += " https://www.apeoplescalendar.org/calendar/events/" + slugifiedEventName
 
         print('posting tweet:')
-        print(tweetBody[:240])
+        print(otd)
         print('\n')
 
         if not testMode:
-            topLevelTweet = ''
-            # if we have image
-            if imgSrc:
-                url = 'https://www.apeoplescalendar.org{}'.format(imgSrc)
-                imgTuple = urllib.request.urlretrieve(url)
-                image = imgTuple[0]
-                try:
-                    media = api.media_upload(image)
-                    api.create_media_metadata(media.media_id, alt_text=imgAltText)
-                    topLevelTweet = api.update_status(status=tweetBody, media_ids=[media.media_id], possibly_sensitive=NSFW)
-                except Exception as error:
-                    # bug in the Tweepy where some image files can't be found
-                    # just do the description
-                    print('posting event photo to twitter caused error, posting text instead')
-                    print(error)
-                    topLevelTweet = api.update_status(status=tweetBody, possibly_sensitive=NSFW)
-            else:
-                #if no image, just do the description
-                topLevelTweet = api.update_status(status=tweetBody, possibly_sensitive=NSFW)
-            try:
-                postDescriptionThread(topLevelTweet.id, description, tweetBody)
-            except Exception as e:
-                print('something horrible happened when trying to post description thread:')
-                print(tweetBody)
-                print(e)
+            postEventToTwitter(apcEvent)
 
 # this function breaks down the event description by comma,
 # then builds up and posts an array of tweets in the form of a thread
+
 def postDescriptionThread(initialTweetId, description, otdStatement):
     # break down description by comma (some sentences could be longer than 240 characters)
     descriptionSentences = sent_tokenize(description)
@@ -95,7 +142,8 @@ def postDescriptionThread(initialTweetId, description, otdStatement):
         # split sentences by comma that isn't part of number (i.e., don't split 1,234)
         splitByComma = re.split(r',(?!\d)', paddedSentence)
         # add comma delimiter back in. if you can't understand this either, blame orestisf on stackoverflow
-        withCommaAddedBackIn = [substr + ',' for substr in splitByComma[:-1]] + [splitByComma[-1]]
+        withCommaAddedBackIn = [
+            substr + ',' for substr in splitByComma[:-1]] + [splitByComma[-1]]
         for clause in withCommaAddedBackIn:
             descriptionByComma.append(clause)
 
@@ -121,9 +169,9 @@ def postDescriptionThread(initialTweetId, description, otdStatement):
     currentTweetIdToReplyTo = initialTweetId
     try:
         for tweet in descriptionTweets:
-            currentTweet = api.update_status(tweet, in_reply_to_status_id=currentTweetIdToReplyTo)
+            currentTweet = api.update_status(
+                tweet, in_reply_to_status_id=currentTweetIdToReplyTo)
             currentTweetIdToReplyTo = currentTweet.id
     except Exception as e:
         print('something horrible happened when trying to post description thread:')
         print(e)
-
